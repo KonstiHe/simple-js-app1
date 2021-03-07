c9f16fbb850e1a73bf890ef074788e8ee9e215d9
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-03-07 17:59:21 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
-let pokemonList [{name:"Bulbasaur", height:"7", types: ["grass","poison"]},{name: "Venusaur", height: "2", types: ["grass","poison"]},
-{name: "Charizard", height: "1.7", types: ["fire","flying"]}];
+let pokemonList=[{name:"Bulbasaur", height:"7", types:["grass","poison"]}, {name: "Venusaur", height: "2", types:["grass","poison"]},
+{name: "Charizard", height:"1.7", types:["fire","flying"]}];
