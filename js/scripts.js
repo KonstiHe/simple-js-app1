9a77d50fa4b63a0c9e8a5c37e3cdeef3aeb4b9d0
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-03-14 10:29:02 +0100

    Update scripts.js
    add a loop with a conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
-let pokemonList=[{name:"Bulbasaur", height:"7", types:["grass","poison"]}, {name: "Venusaur", height: "2", types:["grass","poison"]},
-{name: "Charizard", height:"1.7", types:["fire","flying"]}];
+let pokemonList=[
+  {name:"Bulbasaur", height:"7", types:["grass","poison"]},
+  {name: "Venusaur", height: "2", types:["grass","poison"]},
+  {name: "Charizard", height:"1.7", types:["fire","flying"]}];
+
+let text="<ul>";
+for (let i=0; i<pokemonList.length; i++){
+  if (pokemonList[i].height>"6") {
+    document.write( text+"<li>"+pokemonList[i].name+" (height:"+pokemonList[i].height+")- Wow. That is big!");
+  } else {
+    document.write(text+"<li>"+pokemonList[i].name+" (height:"+pokemonList[i].height+")");
+  }
+  }
