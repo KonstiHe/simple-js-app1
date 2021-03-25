fa38f1dfdb7fc51db824c27c6316e24e1a7b4c05
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-03-25 23:06:52 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name:"Bulbasaur", height:"7", types:["grass","poison"]},
-    {name: "Venusaur", height: "2", types:["grass","poison"]},
-    {name: "Charizard", height:"1.7", types:["fire","flying"]},
-  ];
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-  function getAll() {
-    return pokemonList;
-  }
+let pokemonList = [
+  {name:"Bulbasaur", height:"7", types:["grass","poison"]},
+  {name: "Venusaur", height: "2", types:["grass","poison"]},
+  {name: "Charizard", height:"1.7", types:["fire","flying"]},
+];
 
-  return {
-    add: add,
-    getAll: getAll
-  };
-})();
+function getAll() {pokemonList.forEach(function(xxx){
+if (xxx.height>"6"){
+  document.write( "<p>"+xxx.name+" (height:"+xxx.height+")- Wow. That is big! </p>")
+} else {
+  document.write("<p>"+xxx.name+" (height:"+xxx.height+") </p>");
+}
+})
+}
 
-  function myLoop(pokemon){
-  if (pokemon.height>"6"){
-    document.write( "<p>"+pokemon.name+" (height:"+pokemon.height+")- Wow. That is big! </p>")
-  } else {
-    document.write("<p>"+pokemon.name+" (height:"+pokemon.height+") </p>");
-  }
+function add(pokemon){
+  pokemonList.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
 };
-
-pokemonList.forEach(myLoop);
+})();
+document.write(pokemonRepository.getAll());
