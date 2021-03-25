f796bade3504623d13ffaa62063796cac3c8012b
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-03-25 23:07:00 +0100

    Update scripts.production.js

--- js/scripts.production.js
+++ js/scripts.production.js
@@ -1,14 +1,22 @@
 
-  let pokemonList = [
-    {name:"Bulbasaur", height:"7", types:["grass","poison"]},
-    {name: "Venusaur", height: "2", types:["grass","poison"]},
-    {name: "Charizard", height:"1.7", types:["fire","flying"]},
-  ];
+let pokemonRepository = (function () {
+let pokemonList = [
+  {name:"Bulbasaur", height:"7", types:["grass","poison"]},
+  {name: "Venusaur", height: "2", types:["grass","poison"]},
+  {name: "Charizard", height:"1.7", types:["fire","flying"]},
+];
 
-  pokemonList.forEach(function(xxx){
- if (xxx.height>"6"){
-    document.write( "<p>"+xxx.name+" (height:"+xxx.height+")- Wow. That is big! </p>")
-  } else {
-    document.write("<p>"+xxx.name+" (height:"+xxx.height+") </p>");
-  }
-});
+function getAll() {pokemonList.forEach(function(xxx){
+if (xxx.height>"6"){
+  document.write( "<p>"+xxx.name+" (height:"+xxx.height+")- Wow. That is big! </p>")
+} else {
+  document.write("<p>"+xxx.name+" (height:"+xxx.height+") </p>");
+}
+})
+}
+
+return {
+  getAll: getAll
+};
+})();
+document.write(pokemonRepository.getAll());
