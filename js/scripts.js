d9662b9ff544a6f1827a244f46951f78cd52c023
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-03-25 16:02:26 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-let pokemonList=[
-  {name:"Bulbasaur", height:"7", types:["grass","poison"]},
-  {name: "Venusaur", height: "2", types:["grass","poison"]},
-  {name: "Charizard", height:"1.7", types:["fire","flying"]}];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name:"Bulbasaur", height:"7", types:["grass","poison"]},
+    {name: "Venusaur", height: "2", types:["grass","poison"]},
+    {name: "Charizard", height:"1.7", types:["fire","flying"]},
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
   function myLoop(pokemon){
   if (pokemon.height>"6"){
